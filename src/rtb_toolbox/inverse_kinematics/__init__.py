d6b63a1762f841dc53cc6968d05adb6203700f52
Miguel <miguellukas52@gmail.com>
2023-05-27 22:30:34 -0300

    ik

--- src/rtb_toolbox/inverse_kinematics/__init__.py
+++ src/rtb_toolbox/inverse_kinematics/__init__.py
@@ -6,8 +6,9 @@ from pymoo.optimize import minimize
 from pymoo.termination.default import MaximumGenerationTermination
 
 from rtb_toolbox.forward_kinematics import ForwardKinematic
-from rtb_toolbox.frame import xyz_rotation_matrix, translation_matrix
+from rtb_toolbox.frame import xyz_rotation_matrix, translation_matrix, zyz
 from rtb_toolbox.utils import matrix_log6, inverse_transformation, se3_to_vec, normalize_angle_between_limits
+from scipy.optimize import minimize
 
 
 class InverseKinematicProblem(Problem):
@@ -59,7 +60,7 @@ def evolutive_ik(
     if initial_guess is None:
         initial_guess = np.random.rand(fk.len_links)
 
-    desired_rotation = xyz_rotation_matrix(desired_transformation[3], desired_transformation[4],
+    desired_rotation = zyz(desired_transformation[3], desired_transformation[4],
                                              desired_transformation[5])
 
     desired_pose = sp.matrix2numpy(translation_matrix(desired_transformation[0], desired_transformation[1],
@@ -106,9 +107,7 @@ def position_ik(
         initial_guess=None,
         f_tolerance=1e-7,
         max_iterations=1500,
-        lmbd=.1,
         verbose=False,
-        normalize=False
 ):
     desired_position = np.array([
         [desired_position[0]],
@@ -121,62 +120,37 @@ def position_ik(
 
     theta_i = initial_guess.copy()
 
-    F = f_tolerance + 1
-    i = 0
-
-    while F > f_tolerance and i < max_iterations:
-        P_i = fk.compute_ee_position(theta_i)
+    def cost(thetas):
+        P_i = fk.compute_ee_position(thetas)
         G = P_i - desired_position
-
         F = .5 * G.T @ G
 
-        J_k = fk.compute_jacobian(theta_i)[3:, :]
+        return F
+    
+    res = minimize(
+        cost,
+        theta_i,
+        options={
+            'maxiter': max_iterations,
+            'disp': verbose,
+            'gtol': f_tolerance,
+        },
+        method='BFGS',
+    )
 
-        theta_i -= lmbd * (np.linalg.pinv(J_k) @ G)[:, 0]
+    optimal_theta = res.x
+    f = res.fun
 
-        i += 1
-
-        if verbose:
-            print(f'Iteration {i}, F = {F}')
-
-    error = F > f_tolerance
-
-    if normalize:
-        for i in range(fk.len_links):
-            link_limits = fk.links[i].limits
-
-            theta = normalize_angle_between_limits(
-                theta_i[i],
-                link_limits[0],
-                link_limits[1]
-            )
-
-            theta_i[i] = theta
-
-    return theta_i, desired_position, not error
+    return optimal_theta, desired_position, f
 
 
 def full_ik(
         desired_transformation=None,
         fk: ForwardKinematic = None,
         initial_guess=None,
-        epsilon_wb=1e-5,
-        epsilon_vb=1e-5,
+        epsilon=1e-5,
         max_iterations=1000,
-        lmbd=.1,
-        verbose=False,
-        only_position=False,
-        normalize=True):
-
-    if only_position:
-        return position_ik(
-            desired_position=desired_transformation[:3],
-            fk=fk,
-            initial_guess=initial_guess,
-            f_tolerance=epsilon_vb,
-            max_iterations=max_iterations,
-            lmbd=lmbd,
-            verbose=not verbose)
+        verbose=False,):
 
     # transformation_data = [x, y, z, rx, ry, rz]
     # x, y, z: position of the end effector
@@ -186,7 +160,7 @@ def full_ik(
     if initial_guess is None:
         initial_guess = initial_guess = np.random.rand(6)
 
-    desired_rotation = xyz_rotation_matrix(desired_transformation[3], desired_transformation[4],
+    desired_rotation = zyz(desired_transformation[3], desired_transformation[4],
                                              desired_transformation[5])
 
     desired_pose = sp.matrix2numpy(translation_matrix(desired_transformation[0], desired_transformation[1],
@@ -194,11 +168,8 @@ def full_ik(
 
     theta_i = initial_guess.copy()
 
-    error = True
-    i = 0
-
-    while error and i < max_iterations:
-        htm = fk.compute_ee_transformation_matrix(theta_i)
+    def cost(thetas):
+        htm = fk.compute_ee_transformation_matrix(thetas)
         i_htm = inverse_transformation(htm)
 
         T_bd = i_htm @ desired_pose
@@ -206,19 +177,20 @@ def full_ik(
 
         s = se3_to_vec(log_tbd)
 
-        J = fk.compute_jacobian(theta_i)
+        return np.linalg.norm(s)
 
-        d_theta = np.linalg.pinv(J) @ s
-        theta_i += (lmbd * d_theta)
+    res = minimize(
+        cost,
+        theta_i,
+        options={
+            'maxiter': max_iterations,
+            'disp': verbose,
+            'gtol': epsilon,
+        },
+        method='BFGS',
+    )
 
-        wb_err = np.linalg.norm(s[:3])
-        vb_err = np.linalg.norm(s[3:])
+    optimal_theta = res.x
+    f = res.fun
 
-        error = wb_err > epsilon_wb or vb_err > epsilon_vb
-
-        i += 1
-
-        if verbose:
-            print(f'Iteration {i}, s = {s}')
-
-    return theta_i, desired_pose, np.linalg.norm(s)
+    return optimal_theta, desired_pose, f
