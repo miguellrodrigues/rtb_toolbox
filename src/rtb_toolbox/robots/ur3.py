1a9d3bc81d2bf71126a366f7ae3a4a77cd7ad98d
Miguel Rodrigues <miguellukas52@gmail.com>
2023-11-26 21:15:02 -0300

    .

--- src/rtb_toolbox/robots/ur3.py
+++ src/rtb_toolbox/robots/ur3.py
@@ -5,7 +5,15 @@ from sympy import pi
 from rtb_toolbox.forward_kinematics import ForwardKinematic
 from rtb_toolbox.link import Link
 
-q1, q2, q3, q4, q5, q6 = sp.symbols('q_1 q_2 q_3 q_4 q_5 q_6')
+t = sp.symbols('t')
+q1 = sp.Function('q_1')(t)
+q2 = sp.Function('q_2')(t)
+q3 = sp.Function('q_3')(t)
+q4 = sp.Function('q_4')(t)
+q5 = sp.Function('q_5')(t)
+q6 = sp.Function('q_6')(t)
+
+# q1, q2, q3, q4, q5, q6 = sp.symbols('q_1 q_2 q_3 q_4 q_5 q_6')
 
 I_1 = np.diag([0, -.02, 0])
 I_2 = np.diag([0.13, 0, 0.1157])
@@ -21,13 +29,13 @@ m_4 = .871
 m_5 = .805
 m_6 = .261
 
-j0 = Link([q1,       0, 150,     pi / 2], inertia_tensor=I_1, mass=m_1)
-j1 = Link([q2,       0, 590,     pi    ], inertia_tensor=I_2, mass=m_2)
-j2 = Link([q3,       0, 130,    -pi / 2], inertia_tensor=I_3, mass=m_3)
-j3 = Link([q4, -647.07,   0,    -pi / 2], inertia_tensor=I_4, mass=m_4)
-j4 = Link([q5,       0,   0,     pi / 2], inertia_tensor=I_5, mass=m_5)
-j5 = Link([q6,     -95,   0,     0     ], inertia_tensor=I_6, mass=m_6)
+j0 = Link([q1, 0.15185,        0,  pi / 2], inertia_tensor=I_1, mass=m_1)
+j1 = Link([q2,       0, -0.24355,  0     ], inertia_tensor=I_2, mass=m_2)
+j2 = Link([q3,       0, -0.2132 ,  0     ], inertia_tensor=I_3, mass=m_3)
+j3 = Link([q4, 0.13105,        0,  pi / 2], inertia_tensor=I_4, mass=m_4)
+j4 = Link([q5, 0.08535,        0, -pi / 2], inertia_tensor=I_5, mass=m_5)
+j5 = Link([q6, 0.0921 ,        0,  0     ], inertia_tensor=I_6, mass=m_6)
 
-comau_fk = ForwardKinematic(
+ur3_fk = ForwardKinematic(
 		[j0, j1, j2, j3, j4, j5],
 )
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,21 @@
-from rtb_toolbox.robots.comau import comau_fk
+from rtb_toolbox.robots.ur3 import ur3_fk
 from rtb_toolbox.inverse_kinematics import evolutive_ik, full_ik
+from rtb_toolbox.forward_dynamics import ForwardDynamics
 
 import numpy as np
 from math import pi
+import sympy as sp
 
-theta, f = evolutive_ik(
-	np.array([537.95, -537.95, 295.96, 0, pi, pi / 4]),
-	comau_fk,
+ee_pos = ur3_fk.compute_ee_position(np.random.rand(6))[:, 0]
+
+theta, f = full_ik(
+	np.array([*ee_pos, 0, pi, pi / 4]),
+	ur3_fk,
 	max_iterations=1024
 )
 
+print(' ')
 print(theta, f)
+print(' ')
+
+print(ur3_fk.compute_ee_position(theta)[:, 0], ee_pos)
